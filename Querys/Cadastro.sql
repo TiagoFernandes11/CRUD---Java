5dc2814ee82223f15a43f046bd7818a5194f157d
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-02-13 11:42:49 -0300

    Update Cadastro.sql

--- Querys/Cadastro.sql
+++ Querys/Cadastro.sql
@@ -16,12 +16,20 @@ cargo varchar(20) not null,
 subordinado int(3),
 foto blob,
 celular varchar(12)  not null,
-MFA
-Centro de Custo
-Departamento
-Divisão
-Perfil
+cdc enum('cp','cnp'),
+departamento varchar(20),
+Divisão varchar(5),
+Perfil varchar(30),
+foreign key(id_empresa) references empresa(id)
+);
 
+create table endereco(
+id int(3) primary key auto_increment,
+cep varchar(9) not null,
+rua varchar(50) not null,
+bairro varchar(50) not null,
+numero varchar(6) not null,
+complemento varchar(50)
 );
 
 create table empresa(
@@ -29,9 +37,11 @@ id int(3) primary key auto_increment,
 razao varchar(20) not null,
 fantasia varchar(30),
 cnpj varchar(14) not null,
-ie
-im
-endereco int(3) not null,
-logo blob
+ie varchar(9) not null,
+im varchar(11) not null,
+id_endereco int(3) not null,
+logo blob,
+foreign key (id_endereco) references endereco(id)
 );
+
 
